7c8a551c839607ef4ac7d2e3579bce6de92763ce
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2026-03-26 22:41:25 +0530

    spring data jpa

---
+++ 03-spring-test-04-jpa-crud/src/com/spring/learn/entity/EmployeeEntity.java
@@ -0,0 +1,51 @@
+package com.spring.learn.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //mandatory - creates table inside DB. default table name will be class name.
+@Table(name="Employee1") //optional - to change the table name inside DB.
+public class EmployeeEntity {
+	
+	@Id //mandatory - primary key
+	//@Column(name="EmployeeId") //optional - change column name inside table
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Integer empId;
+	String empName;
+	String role;
+	Double salary;
+	
+	
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+
+}
