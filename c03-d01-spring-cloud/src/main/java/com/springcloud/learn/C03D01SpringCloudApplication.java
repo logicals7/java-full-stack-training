fea3d5131bdfdab534416079f62af994263bc792
Krishan Kant <42918717+logicals7@users.noreply.github.com>
2026-04-13 22:44:38 +0530

    spring-cloud

---
+++ c03-d01-spring-cloud/src/main/java/com/springcloud/learn/C03D01SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C03D01SpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C03D01SpringCloudApplication.class, args);
+    }
+
+}
